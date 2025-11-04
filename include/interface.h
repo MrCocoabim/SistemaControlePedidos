22e10196f4644d36b3293bf5758fb3343a8f4977
MrCocoabim <mrcocoabim021@hotmail.com>
2025-11-04 18:49:15 -0300

    Adicionando os arquivos interface.h e persistencia.h

---
+++ include/interface.h
@@ -0,0 +1,6 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+// Declarações de funções da interface (menus, etc.)
+
+#endif // INTERFACE_H
---
+++ include/persistencia.h
@@ -0,0 +1,6 @@
+#ifndef PERSISTENCIA_H
+#define PERSISTENCIA_H
+
+// Declarações de funções de salvar/carregar CSV
+
+#endif // PERSISTENCIA_H
